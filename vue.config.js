ff822303ac148b04d532bf8c46fa4bb2c7f7dc15
highdanny11 <highdanny11@gmail.com>
2022-01-14 21:40:23 +0800

    修改title

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,9 @@ module.exports = {
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       const [obj] = args;
-      obj.title = '微笑Uibke網';
+      obj.title = '微笑Ubike網';
       obj.meta = {
-        keywords: '台灣Uibke網',
+        keywords: '台灣Ubike網',
         description: '快速搜索附近站點、美食、景點，聰明旅遊輕鬆樂活。'
       };
       obj.icon = 'bikeIcon.png';
